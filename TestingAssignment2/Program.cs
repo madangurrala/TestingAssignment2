590f27fa0691163a50ecd142ed82c8fe634490cf
Madan <mgurrala9086@conestogac.on.ca>
2019-06-19 20:57:30 -0400

    Completed Program

--- TestingAssignment2/Program.cs
+++ TestingAssignment2/Program.cs
@@ -9,7 +9,6 @@ namespace TestingAssignment2
     class Program
     {
        
-
         static void Main(string[] args)
         {
 
@@ -22,6 +21,67 @@ namespace TestingAssignment2
 
         public void TriangleMenu()
         {
+            bool menuOptions = true;
+
+            while (menuOptions == true)
+            {
+
+                Console.WriteLine("Please choose the options below:\n");
+                Console.WriteLine("1.Enter triangle dimensions");
+                Console.WriteLine("2.Exit\n");
+
+                string input = Console.ReadLine();
+
+                if (input != "1" &
+                    input != "2")
+                {
+                    Console.WriteLine("\nOops!! You have selected and incorrect Option. Please enter either " +
+                       "Number 1 or 2 on keyboard.\n");
+                }
+                else
+                {
+                    menuOptions = true;
+                }
+
+                int choice, firstNumber, SecondNumber, thirdNumber;
+
+                bool selectedOption = !int.TryParse(input, out choice);
+
+                switch (choice)
+                {
+
+                    case 1:
+
+                        do
+                        {
+                            Console.Write("\nPlease enter the first Integer:");
+                            input = Console.ReadLine();
+                        }
+                        while (!int.TryParse(input, out firstNumber));
+
+                        do
+                        {
+                            Console.Write("\nPlease enter the second Integer:");
+                            input = Console.ReadLine();
+                        } while (!int.TryParse(input, out SecondNumber));
+
+                        do
+                        {
+                            Console.Write("\nPlease enter the third Integer:");
+                            input = Console.ReadLine();
+                        } while (!int.TryParse(input, out thirdNumber));
+
+                        Console.WriteLine(TriangleSolver.Analyze(firstNumber, SecondNumber, thirdNumber) +
+                                             "\n--------------------------------------------------");
+                        break;
+
+                    case 2:
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+            }
 
         }
     }
--- TestingAssignment2/TriangleSolver.cs
+++ TestingAssignment2/TriangleSolver.cs
@@ -8,8 +8,6 @@ namespace TestingAssignment2
 {
     public static class TriangleSolver
     {
-
-
         public static string Analyze(int firstNumber, int secondNumber, int thirdNumber)
         {
             string result = string.Empty;
@@ -19,24 +17,20 @@ namespace TestingAssignment2
             tLeft = secondNumber + thirdNumber;
             tBase = thirdNumber + firstNumber;
 
-
-            if(tRight > thirdNumber && tLeft > 
-                firstNumber && tBase > secondNumber) {
-
-                Console.WriteLine("\nThe given numbers forms a Triangle\n");
-
+            if(tRight > thirdNumber && tLeft > firstNumber && tBase > secondNumber)
+            {
                 if(firstNumber == secondNumber && firstNumber == thirdNumber)
                 {
-                    result = "The given numbers forms an equilateral Triangle";
+                    result = "The given numbers form a Triangle and it is an equilateral Triangle";
                 }
 
                 else if ((firstNumber == secondNumber) || (firstNumber == thirdNumber) || (thirdNumber == secondNumber))
                 {
-                    result = "The given numbers forms an isosceles triangle";
+                    result = "The given numbers form a Triangle and it is an isosceles triangle";
                 }
                 else if((firstNumber != secondNumber) && (secondNumber != thirdNumber) )
                 {
-                    result = "The given numbers forms a scalene triangle";
+                    result = "The given numbers form a Triangle and it is a scalene triangle";
                 }
 
             }
